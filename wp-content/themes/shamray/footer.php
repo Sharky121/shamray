35a9a24dbf87b0776cee578af60f7f2bf0884d92
Sharky <Sharky121@mail.ru>
2020-10-30 01:22:04 +0300

    fix: Поправлена главная страница

--- wp-content/themes/shamray/footer.php
+++ wp-content/themes/shamray/footer.php
@@ -1,17 +1,5 @@
 </div>
 <footer class="main-footer">
-  <div class="main-footer__contacts main-footer-contacts">
-    <div class="container">
-      <ul class="main-footer-contacts__list">
-        <li class="main-footer-contacts__item">
-          <h3>ООО "Вентмаркет"</h3>
-          <p>390023, Рязань, проезд Яблочкова, 6, стр. 1, помещение Н12, офис 1</p>
-          <p>+7 (495) 145-78-15</p>
-          <p>+7 (910) 642-19-85</p>
-        </li>
-      </ul>
-    </div>
-  </div>
   <div class="main-footer__copyright">
     <div class="container">
       <p>© 2000 - 2020 ООО "Вентмаркет"</p>
--- wp-content/themes/shamray/index.php
+++ wp-content/themes/shamray/index.php
@@ -1,98 +1,150 @@
 <?php get_header(); ?>
   <main class="page-content index-page">
+    <h1 class="visually-hidden">Металлические изделия из перфорированного листа</h1>
     <section class="index-page__slider index-slider">
       <div class="index-slider__background"></div>
       <div class="container index-slider__container">
         <h2 class="index-slider__title">Металлические изделия <br> из перфорированного листа</h2>
-
         <ul class="index-slider__list features__list">
           <li class="features__item">Изготовление по вашим размерам</li>
           <li class="features__item">Штучно или в крупных объемах</li>
           <li class="features__item">Минимальные сроки изготовления</li>
           <li class="features__item">Доставка транспортными компаниями</li>
         </ul>
-
         <a class="index-slider__btn btn" href="">Оставить заявку</a>
       </div>
     </section>
-
-    <section class="index-page__section index-page__our-production our-production">
-      <div class="container our-production__container">
-        <div class="our-production__content">
-          <h2 class="our-production__title">Наша продукция</h2>
-
-          <p>В наличии — широкий ассортимент стандартных металлических изделий. Мы готовы изготовить любую декоративную решетку по вашим размерам. </p>
-          <p>Максимальные размеры изделия — <b>2000 х 1000 мм</b>. При необходимости возможно изготовление более объемной решетки из нескольких составных частей.</p>
-          <p>Изделия из перфорированной стали окрашиваем в любой цвет по выбору заказчика, располагаем возможностью печати на решетках цифровых изображений.</p>
-        </div>
-        <div class="our-production__carousel owl-carousel owl-theme">
-          <div class="item">
-            <img src="<?php bloginfo('template_directory')?>/img/slide_1.jpg" alt="">
-          </div>
-          <div class="item">
-            <img src="<?php bloginfo('template_directory')?>/img/slide_2.jpg" alt="">
-          </div>
-          <div class="item">
-            <img src="<?php bloginfo('template_directory')?>/img/slide_3.jpg" alt="">
-          </div>
-          <div class="item">
-            <img src="<?php bloginfo('template_directory')?>/img/slide_4.jpg" alt="">
-          </div>
-          <div class="item">
-            <img src="<?php bloginfo('template_directory')?>/img/slide_5.jpg" alt="">
-          </div>
-        </div>
-      </div>
-    </section>
-
     <section class="index-page__section index-page__advantages advantages">
       <div class="container advantages__container">
-        <h2 class="advantages__title">Наши преимущества</h2>
+        <h2 class="section-title">
+          <span>Почему нас выбирают?</span>
+          Преимущества
+        </h2>
+        <p class="advantages__subtitle">Вы можете выбрать любую модель из каталога, а также заказать индивидуальный проект с нуля</p>
 
         <ul class="advantages__list">
           <li class="advantages__item">
             <img class="advantages__img" src="<?php bloginfo('template_directory')?>/img/quality.svg" width="60" height="60" alt="">
-            <h3 class="advantages__subtitle">Высокое качество запатентованных изделий собственного производства</h3>
+            <h3 class="advantages__description">Высокое качество запатентованных <br> изделий собственного производства</h3>
           </li>
           <li class="advantages__item">
             <img class="advantages__img" src="<?php bloginfo('template_directory')?>/img/sketch.svg" width="60" height="60" alt="">
-            <h3 class="advantages__subtitle">Возможность изготовления по эскизам заказчика</h3>
+            <h3 class="advantages__description">Возможность изготовления <br> по эскизам заказчика</h3>
           </li>
           <li class="advantages__item">
             <img class="advantages__img" src="<?php bloginfo('template_directory')?>/img/choices.svg" width="60" height="60" alt="">
-            <h3 class="advantages__subtitle">Возможность выбора перфорации</h3>
+            <h3 class="advantages__description">Возможность <br> выбора перфорации</h3>
           </li>
           <li class="advantages__item">
             <img class="advantages__img" src="<?php bloginfo('template_directory')?>/img/paint-swatch.svg" width="60" height="60" alt="">
-            <h3 class="advantages__subtitle">Возможность выбора окраски по RAL</h3>
+            <h3 class="advantages__description">Возможность выбора <br> окраски по RAL</h3>
           </li>
           <li class="advantages__item">
             <img class="advantages__img" src="<?php bloginfo('template_directory')?>/img/delivery-truck.svg" width="60" height="60" alt="">
-            <h3 class="advantages__subtitle">Доставка транспортными компаниями по всей России</h3>
+            <h3 class="advantages__description">Доставка транспортными компаниями <br> по всей России</h3>
           </li>
           <li class="advantages__item">
             <img class="advantages__img" src="<?php bloginfo('template_directory')?>/img/clock.svg" width="60" height="60" alt="">
-            <h3 class="advantages__subtitle">Кратчайшие сроки изготовления</h3>
+            <h3 class="advantages__description">Кратчайшие <br> сроки изготовления</h3>
           </li>
         </ul>
       </div>
     </section>
-
     <section class="index-page__section index-page__about index-about">
       <div class="container index-about__container">
-        <div class="index-about__img">
-          <img src="<?php bloginfo('template_directory')?>/img/about.jpg" width="400" alt="">
-        </div>
-        <div class="index-about__content">
-          <h2 class="index-about__title">О компании</h2>
-
-          <p>Предприятие «ИП Шамрай Виктор Павлович» изготавливает металлические экраны на батарею отопления, декоративные вентиляционные решетки, коробы для кондиционеров и другие изделия из металла.
-            Наша компания успешно работает в отрасли уже более пятнадцати лет, с 2000 года.</p>
-          <p>Основное производство и главный офис компании расположены в городе Рязань.
-            Компания “Шамрай” является лидером по производству решеток и экранов, где используется перфорация из металла.</p>
+        <h2 class="section-title">
+          <span>Вентмаркет</span>
+          О компании
+        </h2>
+        <p class="index-about__subtitle">Компания ООО «Вентмаркет» является лидером по производству решеток и экранов, где используется перфорация из металла.</p>
+        <div class="index-about__row">
+          <div class="index-about__img">
+            <img src="<?php bloginfo('template_directory')?>/img/about.jpg" width="400" alt="">
+          </div>
+          <div class="index-about__content">
+            <h3>Наша компания успешно работает в отрасли уже более 15 лет</h3>
+            <h2>Вся продукция защищена сертификатами качества и патентами</h2>
+            <p>Предприятие ООО «Вентмаркет» изготавливает металлические экраны на батареи отопления, декоративные вентиляционные решетки, коробы для кондиционеров и другие изделия из металла.</p>
+            <a class="btn btn--white" href="">Подробней</a>
+            <a class="btn btn--white" href="">Продукция</a>
+          </div>
         </div>
       </div>
     </section>
+    <section class="index-page__section index-page__contacts index-contacts">
+      <div class="container index-about__container">
+        <h2 class="section-title">
+          <span>Контакты</span>
+          Бесплатная консультация
+        </h2>
+        <p class="index-about__subtitle">
+          Для получения информации вы можете связаться с нами. <br> Мы будем рады помочь вам!
+        </p>
+
+        <ul class="index-contacts__list">
+          <li class="index-contacts__item index-contacts-item">
+            <img class="index-contacts-item__img" src="<?php bloginfo('template_directory')?>/img/mobile.svg" width="40" height="40" alt="">
+            <h3 class="index-contacts-item__title">Позвоните нам</h3>
+            <p class="index-contacts-item__description">
+              <a href="">+7 (495) 145-78-15</a>
+              <br>
+              <a href="">+7 (910) 642-19-85</a>
+            </p>
+          </li>
+          <li class="index-contacts__item index-contacts-item">
+            <img class="index-contacts-item__img" src="<?php bloginfo('template_directory')?>/img/mail.svg" width="40" height="40" alt="">
+            <h3 class="index-contacts-item__title">Напишите нам</h3>
+            <p class="index-contacts-item__description">
+              <a href="">domresh@mail.ru</a>
+            </p>
+          </li>
+          <li class="index-contacts__item index-contacts-item">
+            <img class="index-contacts-item__img" src="<?php bloginfo('template_directory')?>/img/location.svg" width="40" height="40" alt="">
+            <h3 class="index-contacts-item__title">Наш адрес</h3>
+            <p class="index-contacts-item__description">
+              390023, Рязань, проезд Яблочкова, 6, стр. 1, помещение Н12, офис 1
+            </p>
+          </li>
+        </ul>
+
+        <div class="index-contacts__map">
+
+        </div>
+<!--        <div class="index-contacts__separator">-->
+<!--          <p>ИЛИ</p>-->
+<!--        </div>-->
+      </div>
+    </section>
+
+<!--    <section class="index-page__section index-page__our-production our-production">-->
+<!--      <div class="container our-production__container">-->
+<!--        <div class="our-production__content">-->
+<!--          <h2 class="our-production__title">Наша продукция</h2>-->
+<!---->
+<!--          <p>В наличии — широкий ассортимент стандартных металлических изделий. Мы готовы изготовить любую декоративную решетку по вашим размерам. </p>-->
+<!--          <p>Максимальные размеры изделия — <b>2000 х 1000 мм</b>. При необходимости возможно изготовление более объемной решетки из нескольких составных частей.</p>-->
+<!--          <p>Изделия из перфорированной стали окрашиваем в любой цвет по выбору заказчика, располагаем возможностью печати на решетках цифровых изображений.</p>-->
+<!--        </div>-->
+<!--        <div class="our-production__carousel owl-carousel owl-theme">-->
+<!--          <div class="item">-->
+<!--            <img src="--><?php //bloginfo('template_directory')?><!--/img/slide_1.jpg" alt="">-->
+<!--          </div>-->
+<!--          <div class="item">-->
+<!--            <img src="--><?php //bloginfo('template_directory')?><!--/img/slide_2.jpg" alt="">-->
+<!--          </div>-->
+<!--          <div class="item">-->
+<!--            <img src="--><?php //bloginfo('template_directory')?><!--/img/slide_3.jpg" alt="">-->
+<!--          </div>-->
+<!--          <div class="item">-->
+<!--            <img src="--><?php //bloginfo('template_directory')?><!--/img/slide_4.jpg" alt="">-->
+<!--          </div>-->
+<!--          <div class="item">-->
+<!--            <img src="--><?php //bloginfo('template_directory')?><!--/img/slide_5.jpg" alt="">-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </section>-->
+
   </main>
 <?php get_footer();?>
 
